ac4e75face5f561763a34ac171492b2e27bec385
arunkalher <89917240+arunkalher@users.noreply.github.com>
2023-12-18 13:07:18 +0530

    #updates

--- client.py
+++ client.py
@@ -1,34 +1,33 @@
-import socket 
+import socket
 import threading
 
-nickName=input("Choose your Nick Name: ")
+nickName = input("Choose your Nick Name: ")
 
-client=socket.socket()
-client.connect(('localhost',12345))
+client = socket.socket()
+client.connect(("localhost", 12345))
+
 
 def receive():
     while True:
-
         try:
-            msg=client.recv(1024).decode()
-            if msg=="NICK NAME:":
+            msg = client.recv(1024).decode()
+            if msg == "NICK NAME:":
                 client.send(nickName.encode())
             else:
                 print(msg)
         except:
-
             print("Error occured")
             client.close()
 
             break
 
+
 def write():
     while True:
-        print(">>>",end="")
-        msg=f"{nickName}: {input()}"
+        print(">>>", end="")
+        msg = f"{nickName}: {input()}"
         client.send(msg.encode())
 
 
 threading.Thread(target=receive).start()
 threading.Thread(target=write).start()
-
--- server.py
+++ server.py
@@ -1,39 +1,46 @@
 import threading
 import socket
-clients=[] # tuple with first element as a soc obj and second element is client name ( provided by client )
-nick_names=[]
-Socket=socket.socket()
-Socket.bind(('localhost',12345)) # bind socket to ip add- localhost and arbitrary available port no
 
-Socket.listen(5)  
+clients = (
+    []
+)  # tuple with first element as a soc obj and second element is client name ( provided by client )
+nick_names = []
+Socket = socket.socket()
+Socket.bind(
+    ("localhost", 12345)
+)  # bind socket to ip add- localhost and arbitrary available port no
+
+Socket.listen(5)
+
 
 def broadcast(msg):
     for client in clients:
         client.send(msg)
 
+
 # handle msg from clients
 def msg_handler(client):
-
     while True:
         try:
             # broadcast msg
-            msg=client.recv(1024)
+            msg = client.recv(1024)
             broadcast(msg)
-        except Exception as e :
-            indx=clients.index(client)
+        except Exception as e:
+            indx = clients.index(client)
             clients.remove(client)
             client.close()
-           
+
             broadcast(f"{nick_names[indx]} left due to {e}".encode())
             nick_names.remove(nick_names[indx])
             break
 
+
 def receive():
     while True:
-        client,client_ip=Socket.accept()
+        client, client_ip = Socket.accept()
         print(f"Connected with {client_ip}")
         client.send("NICK NAME:".encode())
-        nickName=client.recv(1024).decode()
+        nickName = client.recv(1024).decode()
         clients.append(client)
         nick_names.append(nickName)
 
@@ -42,7 +49,8 @@ def receive():
         broadcast(f"Nick Name {nickName} joined".encode())
 
         client.send("Connected to Server!!".encode())
-        thread=threading.Thread(target=msg_handler,args=(client,))
+        thread = threading.Thread(target=msg_handler, args=(client,))
         thread.start()
+
 
 receive()
